01c0e2abe09b68c1b9489eeec4666534336bc5e6
Alisa Knoebl <knal1011@h-ka.de>
2026-06-19 14:40:11 +0200

    zimmer.go hinzugefügt

---
+++ internal/model/zimmer.go
@@ -0,0 +1,10 @@
+package model
+
+type Zimmer struct {
+	ID           uint    `gorm:"primaryKey;autoIncrement"    json:"id"`
+	Preis        float64 `gorm:"type:decimal(10,2);not null" json:"preis"`
+	Zimmernummer string  `gorm:"not null"                    json:"zimmernummer"`
+	HotelID      uint    `gorm:"column:hotel_id;index"       json:"hotel_id"`
+}
+
+func (Zimmer) TableName() string { return "hotel.zimmer" }
