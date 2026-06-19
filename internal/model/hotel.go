8aa585db97e9966555b5f7fd7d002fef42899b8f
Alisa Knoebl <knal1011@h-ka.de>
2026-06-19 14:39:24 +0200

    hotel.go hinzugefügt

---
+++ internal/model/hotel.go
@@ -0,0 +1,15 @@
+package model
+
+import "time"
+
+type Hotel struct {
+	ID           uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Version      int       `gorm:"default:0"               json:"version"`
+	Name         string    `gorm:"not null;index"          json:"name"`
+	Erzeugt      time.Time `gorm:"column:erzeugt"          json:"erzeugt"`
+	Aktualisiert time.Time `gorm:"column:aktualisiert"     json:"aktualisiert"`
+	Standort     *Standort `gorm:"foreignKey:HotelID;constraint:OnDelete:CASCADE" json:"standort,omitempty"`
+	Zimmer       []Zimmer  `gorm:"foreignKey:HotelID;constraint:OnDelete:CASCADE" json:"zimmer,omitempty"`
+}
+
+func (Hotel) TableName() string { return "hotel.hotel" }
