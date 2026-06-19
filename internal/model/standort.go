3fb18289002f32d17a980363c0a8c71b9e7cbf51
Alisa Knoebl <knal1011@h-ka.de>
2026-06-19 14:39:50 +0200

    standort.go hinzugefügt

---
+++ internal/model/standort.go
@@ -0,0 +1,13 @@
+package model
+
+type Standort struct {
+	ID         uint   `gorm:"primaryKey;autoIncrement"    json:"id"`
+	Strasse    string `gorm:"not null"                    json:"strasse"`
+	Hausnummer string `gorm:"not null"                    json:"hausnummer"`
+	PLZ        string `gorm:"column:plz;not null;index"   json:"plz"`
+	Ort        string `gorm:"not null"                    json:"ort"`
+	Land       string `gorm:"not null"                    json:"land"`
+	HotelID    uint   `gorm:"column:hotel_id;uniqueIndex" json:"hotel_id"`
+}
+
+func (Standort) TableName() string { return "hotel.standort" }
